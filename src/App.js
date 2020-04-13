3f6e33ab40b30d69f3caacb39fa0e44531637ffa
1659387-lucassimon <1659387-lucassimon@users.noreply.gitlab.com>
2020-04-13 20:56:05 -0300

    2.0

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1.0
+          Learn React 2.0
         </a>
       </header>
     </div>
