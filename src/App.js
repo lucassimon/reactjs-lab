d4fc1a141f95b3be3b54ef8efc8d37bdb44d9422
1659387-lucassimon <1659387-lucassimon@users.noreply.gitlab.com>
2020-04-14 09:56:01 -0300

    Change app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2.0
+          Learn React change app
         </a>
       </header>
     </div>
